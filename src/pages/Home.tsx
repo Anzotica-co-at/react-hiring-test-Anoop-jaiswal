8bf5152ecea0c504ac1ad40666602a2b24537d3a
ANOOP <connect.anoopjaiswal@gmail.com>
2025-07-01 23:14:59 +0530

    Set up initial project structure: components, pages, layouts with placeholder files

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import MainLayout from "../layouts/MainLayout";
+import Carousel from "../components/Carousel/Carousel";
+
+export default function Home() {
+  return (
+    <MainLayout>
+      <Carousel />
+    </MainLayout>
+  );
+}
